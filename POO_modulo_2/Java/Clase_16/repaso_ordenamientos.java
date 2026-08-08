95df163e66b8e25763183a5ec08ded226d837905
MultyPower23 <juanestebancirogallego9@gmail.com>
2026-08-08 13:55:50 -0500

    Clase 16: Ordenamientos y matrices

---
+++ POO_modulo_2/Java/Clase_16/repaso_ordenamientos.java
@@ -0,0 +1,41 @@
+package Tecnica.POO_modulo_2.Java.Clase_16;
+
+import java.util.Scanner;
+
+public class repaso_ordenamientos {
+  public static void main(String[] args) {
+    // DECLARACION DE VARIABLES
+    int arreglo[], nElementos;
+    Scanner sc = new Scanner(System.in);
+
+    // INGRESAR CANTIDAD DE ELEMENTOS
+    System.out.print("Ingrese la cantidad de elementos del arreglo: ");
+    nElementos = sc.nextInt();
+    arreglo = new int[nElementos];
+
+    // CARGAR ARREGLO
+    for (int i = 0; i < nElementos; i++) {
+      System.out.print("Ingrese el elemento " + (i + 1) + ": ");
+      arreglo[i] = sc.nextInt();
+      // arreglo[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el
+      // elemento " + (i +
+      // 1)));
+    }
+
+    // IMPRIMIR ARREGLO
+    System.out.println("\nArreglo Original Desordenado:");
+    for (int i = 0; i < nElementos; i++) {
+      System.out.print(arreglo[i] + ", ");
+    }
+
+    // ORDENAR ARREGLO
+    for (int i = 0; i < arreglo.length - 1; i++) {
+      for (int j = 0; j < (arreglo.length - 1 - i); j++) {
+        if (arreglo[j] > arreglo[j + 1]) {
+
+        }
+      }
+    }
+    sc.close();
+  }
+}
